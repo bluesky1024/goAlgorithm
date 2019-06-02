e8be8eb23bc91fb2b2a3d3912f8d4dd519f77a9e
bluesky1024 <fangmeng_buaa@163.com>
2019-06-02 15:00:50 +0800

    广度优先搜索：模拟扫雷游戏里电脑的处理逻辑

--- breadth_search/breadth_search.go
+++ breadth_search/breadth_search.go
@@ -109,3 +109,173 @@ func OrangesRotting(grid [][]int) int {
 	}
 	return res
 }
+
+/*问题*/
+/*
+让我们一起来玩扫雷游戏！
+
+给定一个代表游戏板的二维字符矩阵。 'M' 代表一个未挖出的地雷，'E' 代表一个未挖出的空方块，'B' 代表没有相邻（上，下，左，右，和所有4个对角线）地雷的已挖出的空白方块，数字（'1' 到 '8'）表示有多少地雷与这块已挖出的方块相邻，'X' 则表示一个已挖出的地雷。
+
+现在给出在所有未挖出的方块中（'M'或者'E'）的下一个点击位置（行和列索引），根据以下规则，返回相应位置被点击后对应的面板：
+
+如果一个地雷（'M'）被挖出，游戏就结束了- 把它改为 'X'。
+如果一个没有相邻地雷的空方块（'E'）被挖出，修改它为（'B'），并且所有和其相邻的方块都应该被递归地揭露。
+如果一个至少与一个地雷相邻的空方块（'E'）被挖出，修改它为数字（'1'到'8'），表示相邻地雷的数量。
+如果在此次点击中，若无更多方块可被揭露，则返回面板。
+
+
+示例 1：
+
+输入:
+
+[['E', 'E', 'E', 'E', 'E'],
+ ['E', 'E', 'M', 'E', 'E'],
+ ['E', 'E', 'E', 'E', 'E'],
+ ['E', 'E', 'E', 'E', 'E']]
+
+Click : [3,0]
+
+输出:
+
+[['B', '1', 'E', '1', 'B'],
+ ['B', '1', 'M', '1', 'B'],
+ ['B', '1', '1', '1', 'B'],
+ ['B', 'B', 'B', 'B', 'B']]
+
+解释:
+
+示例 2：
+
+输入:
+
+[['B', '1', 'E', '1', 'B'],
+ ['B', '1', 'M', '1', 'B'],
+ ['B', '1', '1', '1', 'B'],
+ ['B', 'B', 'B', 'B', 'B']]
+
+Click : [1,2]
+
+输出:
+
+[['B', '1', 'E', '1', 'B'],
+ ['B', '1', 'X', '1', 'B'],
+ ['B', '1', '1', '1', 'B'],
+ ['B', 'B', 'B', 'B', 'B']]
+
+解释:
+
+
+
+注意：
+
+输入矩阵的宽和高的范围为 [1,50]。
+点击的位置只能是未被挖出的方块 ('M' 或者 'E')，这也意味着面板至少包含一个可点击的方块。
+输入面板不会是游戏结束的状态（即有地雷已被挖出）。
+简单起见，未提及的规则在这个问题中可被忽略。例如，当游戏结束时你不需要挖出所有地雷，考虑所有你可能赢得游戏或标记方块的情况。
+*/
+/*思路*/
+/*
+其实就是模拟点击了一次扫雷游戏之后，游戏内部的判断逻辑
+采用广度优先搜索，从被点击的位置开始，递归上下左右全方位扫描
+指定扫描顺序上下左右
+1.如果周围都是'E'或者数字，则可以将该处设置成'B'，并且向上下左右延伸
+2.如果周围有雷，更改该处为数字
+3.如果某点处不是'E'或者'M'，说明该点已经搜索过，不需要再处理
+*/
+
+func updateBoard(board [][]byte, click []int) [][]byte {
+	//判断是否中奖了
+	if board[click[0]][click[1]] == 'M' {
+		board[click[0]][click[1]] = 'X'
+		return board
+	}
+
+	//没中，也就是'E'，遍历该点周围的点
+	var checkRes byte
+	var cnt byte = 0
+	lPos := click[1] - 1
+	rPos := click[1] + 1
+	uPos := click[0] - 1
+	dPos := click[0] + 1
+	l := lPos >= 0
+	r := rPos <= len(board[click[0]])-1
+	u := uPos >= 0
+	d := dPos <= len(board)-1
+	//LU
+	if l && u && board[uPos][lPos] == 'M' {
+		cnt++
+	}
+	//U
+	if u && board[uPos][click[1]] == 'M' {
+		cnt++
+	}
+	//RU
+	if r && u && board[uPos][rPos] == 'M' {
+		cnt++
+	}
+	//L
+	if l && board[click[0]][lPos] == 'M' {
+		cnt++
+	}
+	//R
+	if r && board[click[0]][rPos] == 'M' {
+		cnt++
+	}
+	//LD
+	if l && d && board[dPos][lPos] == 'M' {
+		cnt++
+	}
+	//D
+	if d && board[dPos][click[1]] == 'M' {
+		cnt++
+	}
+	//RD
+	if r && d && board[dPos][rPos] == 'M' {
+		cnt++
+	}
+	if cnt == 0 {
+		checkRes = 'B'
+	} else {
+		checkRes = cnt + '0'
+	}
+
+	board[click[0]][click[1]] = checkRes
+	if checkRes != 'B' {
+		return board
+	}
+
+	//碰到'B',再遍历click一圈
+	//LU
+	if l && u && (board[uPos][lPos] == 'M' || board[uPos][lPos] == 'E') {
+		board = updateBoard(board, []int{uPos, lPos})
+	}
+	//U
+	if u && (board[uPos][click[1]] == 'M' || board[uPos][click[1]] == 'E') {
+		board = updateBoard(board, []int{uPos, click[1]})
+	}
+	//RU
+	if r && u && (board[uPos][rPos] == 'M' || board[uPos][rPos] == 'E') {
+		board = updateBoard(board, []int{uPos, rPos})
+	}
+	//L
+	if l && (board[click[0]][lPos] == 'M' || board[click[0]][lPos] == 'E') {
+		board = updateBoard(board, []int{click[0], lPos})
+	}
+	//R
+	if r && (board[click[0]][rPos] == 'M' || board[click[0]][rPos] == 'E') {
+		board = updateBoard(board, []int{click[0], rPos})
+	}
+	//LD
+	if l && d && (board[dPos][lPos] == 'M' || board[dPos][lPos] == 'E') {
+		board = updateBoard(board, []int{dPos, lPos})
+	}
+	//D
+	if d && (board[dPos][click[1]] == 'M' || board[dPos][click[1]] == 'E') {
+		board = updateBoard(board, []int{dPos, click[1]})
+	}
+	//RD
+	if r && d && (board[dPos][rPos] == 'M' || board[dPos][rPos] == 'E') {
+		board = updateBoard(board, []int{dPos, rPos})
+	}
+	return board
+}
