dc67bc34091c89ccc885b242c3e523b98564425e
bluesky1024 <fangmeng_buaa@163.com>
2019-08-12 15:21:09 +0800

    链表：奇偶分离

--- linked_list/linked_list_test.go
+++ linked_list/linked_list_test.go
@@ -67,3 +67,29 @@ func TestSwapPairs(t *testing.T) {
 	fmt.Println("after swap")
 	a.Prinf()
 }
+
+func TestOddEvenList(t *testing.T) {
+	a := &ListNode{
+		Val: 1,
+		Next: &ListNode{
+			Val: 2,
+			Next: &ListNode{
+				Val: 3,
+				Next: &ListNode{
+					Val: 4,
+					Next: &ListNode{
+						Val: 5,
+						Next: &ListNode{
+							Val:  6,
+							Next: nil,
+						},
+					},
+				},
+			},
+		},
+	}
+	a.Prinf()
+	a = OddEvenList(a)
+	fmt.Println("after odd")
+	a.Prinf()
+}
