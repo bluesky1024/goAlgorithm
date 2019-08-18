7ea91d287798d877c6245b870763f5b7eb0f0cd5
bluesky1024 <fangmeng_buaa@163.com>
2019-08-18 15:14:16 +0800

    单向链表设计

--- design/design.go
+++ design/design.go
@@ -2,6 +2,7 @@ package design
 
 import (
 	"container/list"
+	"fmt"
 )
 
 /*问题*/
@@ -293,3 +294,210 @@ func (this *MyHashMap) Remove(key int) {
 		}
 	}
 }
+
+/*问题*/
+/*
+设计链表的实现。您可以选择使用单链表或双链表。单链表中的节点应该具有两个属性：val 和 next。val 是当前节点的值，next 是指向下一个节点的指针/引用。如果要使用双向链表，则还需要一个属性 prev 以指示链表中的上一个节点。假设链表中的所有节点都是 0-index 的。
+
+在链表类中实现这些功能：
+
+get(index)：获取链表中第 index 个节点的值。如果索引无效，则返回-1。
+addAtHead(val)：在链表的第一个元素之前添加一个值为 val 的节点。插入后，新节点将成为链表的第一个节点。
+addAtTail(val)：将值为 val 的节点追加到链表的最后一个元素。
+addAtIndex(index,val)：在链表中的第 index 个节点之前添加值为 val  的节点。如果 index 等于链表的长度，则该节点将附加到链表的末尾。如果 index 大于链表长度，则不会插入节点。如果index小于0，则在头部插入节点。
+deleteAtIndex(index)：如果索引 index 有效，则删除链表中的第 index 个节点。
+
+
+示例：
+
+MyLinkedList linkedList = new MyLinkedList();
+linkedList.addAtHead(1);
+linkedList.addAtTail(3);
+linkedList.addAtIndex(1,2);   //链表变为1-> 2-> 3
+linkedList.get(1);            //返回2
+linkedList.deleteAtIndex(1);  //现在链表是1-> 3
+linkedList.get(1);            //返回3
+
+
+提示：
+
+所有val值都在 [1, 1000] 之内。
+操作次数将在  [1, 1000] 之内。
+请不要使用内置的 LinkedList 库。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/design-linked-list
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+/*思路*/
+/*
+初始化的时候有一个节点，此时val==-1表示这个节点是无效的，即list是空list
+*/
+type MyLinkedList struct {
+	len   int
+	nodes *MyLinkedNode
+}
+
+type MyLinkedNode struct {
+	val  int
+	next *MyLinkedNode
+}
+
+/** Initialize your data structure here. */
+func ConstructorMyLinkedList() MyLinkedList {
+	return MyLinkedList{
+		nodes: nil,
+		len:   0,
+	}
+}
+
+func (this *MyLinkedList) Print() {
+	if this.nodes == nil {
+		fmt.Println("empty list")
+	}
+	cur := this.nodes
+	for {
+		if cur == nil {
+			break
+		}
+		fmt.Println(cur.val)
+		cur = cur.next
+	}
+}
+
+/** Get the value of the index-th node in the linked list. If the index is invalid, return -1. */
+func (this *MyLinkedList) Get(index int) int {
+	if index < 0 || index+1 > this.len {
+		return -1
+	}
+	cur := this.nodes
+	for {
+		if cur == nil {
+			break
+		}
+		if index == 0 {
+			return cur.val
+		}
+		index--
+		cur = cur.next
+	}
+	return -1
+}
+
+/** Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list. */
+func (this *MyLinkedList) AddAtHead(val int) {
+	this.len++
+	if this.nodes == nil {
+		this.nodes = &MyLinkedNode{
+			val:  val,
+			next: nil,
+		}
+		return
+	}
+	newNode := &MyLinkedNode{
+		val:  val,
+		next: this.nodes,
+	}
+	this.nodes = newNode
+	return
+}
+
+/** Append a node of value val to the last element of the linked list. */
+func (this *MyLinkedList) AddAtTail(val int) {
+	this.len++
+	if this.nodes == nil {
+		this.nodes = &MyLinkedNode{
+			val:  val,
+			next: nil,
+		}
+		return
+	}
+	cur := this.nodes
+	for {
+		if cur.next == nil {
+			cur.next = &MyLinkedNode{
+				val:  val,
+				next: nil,
+			}
+			return
+		}
+		cur = cur.next
+	}
+}
+
+/** Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted. */
+func (this *MyLinkedList) AddAtIndex(index int, val int) {
+	this.len++
+	if this.nodes == nil {
+		if index == 0 {
+			this.nodes = &MyLinkedNode{
+				val:  val,
+				next: nil,
+			}
+		}
+		return
+	}
+	newNode := &MyLinkedNode{
+		val:  val,
+		next: nil,
+	}
+	var last *MyLinkedNode = nil
+	cur := this.nodes
+	for {
+		if index == 0 {
+			if last == nil {
+				newNode.next = cur
+				this.nodes = newNode
+			} else {
+				newNode.next = cur
+				last.next = newNode
+			}
+			return
+		}
+		index--
+		last = cur
+		cur = cur.next
+		if cur == nil {
+			if index == 0 {
+				last.next = newNode
+			}
+			return
+		}
+	}
+}
+
+/** Delete the index-th node in the linked list, if the index is valid. */
+func (this *MyLinkedList) DeleteAtIndex(index int) {
+	this.len--
+	if this.nodes == nil {
+		return
+	}
+	var last *MyLinkedNode = nil
+	cur := this.nodes
+	for {
+		if index == 0 {
+			if last == nil {
+				this.nodes = nil
+			} else {
+				last.next = cur.next
+			}
+			return
+		}
+		index--
+		last = cur
+		cur = cur.next
+		if cur == nil {
+			return
+		}
+	}
+}
+
+/**
+ * Your MyLinkedList object will be instantiated and called as such:
+ * obj := Constructor();
+ * param_1 := obj.Get(index);
+ * obj.AddAtHead(val);
+ * obj.AddAtTail(val);
+ * obj.AddAtIndex(index,val);
+ * obj.DeleteAtIndex(index);
+ */
