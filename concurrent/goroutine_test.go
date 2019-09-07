3896e9cb9012940f404781147cfae6e030769b44
bluesky1024 <fangmeng_buaa@163.com>
2019-09-07 15:27:57 +0800

    Go并发编程—温习channel和select

--- concurrent/goroutine_test.go
+++ concurrent/goroutine_test.go
@@ -342,8 +342,8 @@ func startNewNetworkDaemonWithPool() *sync.WaitGroup {
 func init() {
 	//按照书本，理论上应该withPool会比普通的快1000倍，但实际上并没有。。。
 	//daemonStarted := startNetworkDaemon()
-	daemonStarted := startNewNetworkDaemonWithPool()
-	daemonStarted.Wait()
+	//daemonStarted := startNewNetworkDaemonWithPool()
+	//daemonStarted.Wait()
 }
 
 func BenchmarkNetworkRequest(b *testing.B) {
@@ -357,4 +357,162 @@ func BenchmarkNetworkRequest(b *testing.B) {
 		}
 		conn.Close()
 	}
+}
+
+func TestSingleWayChan(t *testing.T) {
+	//单向管道是无法进行逆向操作的，以下操作会报错
+	//writeStream := make(chan<- interface{})
+	//readStream := make(<-chan interface{})
+	//
+	//<-writeStream
+	//readStream <- struct{}{}
+}
+
+func TestGetDataFromChanColosed(t *testing.T) {
+	intStream := make(chan int)
+	close(intStream)
+	integer, ok := <-intStream
+	fmt.Println(integer, ok)
+}
+
+func TestRangeChanClosed(t *testing.T) {
+	intStream := make(chan int)
+	go func() {
+		defer close(intStream)
+		for i := 0; i < 5; i++ {
+			intStream <- i * 10
+		}
+	}()
+	<-intStream
+	//close(chan) 可以让以下range退出循环
+	for integer := range intStream {
+		fmt.Println(integer)
+	}
+}
+
+//close chan 可以用来进行多个协程的同时通知
+func TestCloseChanForInform(t *testing.T) {
+	begin := make(chan interface{})
+	var wg sync.WaitGroup
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			<-begin
+			fmt.Println(i, "has begun")
+		}(i)
+	}
+	fmt.Println("unblocking goroutines...")
+	fmt.Println("before close begin")
+	close(begin)
+	wg.Wait()
+	fmt.Println("after wg wait")
+}
+
+/*
+这种方式很重要
+chanOwner函数仅对外提供了一个只读管道，
+函数内部维护了管道的输入和关闭，
+因此外部调用者只需要知道它应该如何处理阻塞读取和chan的关闭，
+使得系统条理更加清晰
+
+总之---尽量保持channel所有权的范围足够小
+*/
+func TestFuncPackChanInputAndClose(t *testing.T) {
+	chanOwner := func() <-chan int {
+		resultStream := make(chan int, 5)
+		go func() {
+			defer close(resultStream)
+			for i := 0; i < 5; i++ {
+				resultStream <- i * 10
+			}
+		}()
+		return resultStream
+	}
+
+	resultStream := chanOwner()
+	for result := range resultStream {
+		fmt.Println("received: ", result)
+	}
+	fmt.Println("done")
+}
+
+func TestBasicSelect(t *testing.T) {
+	c1 := make(chan interface{})
+	c2 := make(chan interface{})
+	go func() {
+		c1 <- 1
+	}()
+	go func() {
+		c2 <- struct{}{}
+	}()
+	time.Sleep(1 * time.Microsecond)
+	select {
+	case <-c1:
+		fmt.Println("c1 turn do something")
+	case <-c2:
+		fmt.Println("c2 turn do something")
+	default:
+		fmt.Println("get no chan")
+	}
+}
+
+func TestBasicSelectWithoutDefault(t *testing.T) {
+	c1 := make(chan interface{})
+	c2 := make(chan interface{})
+	go func() {
+		time.Sleep(1 * time.Second)
+		c1 <- 1
+	}()
+	go func() {
+		time.Sleep(1 * time.Second)
+		c2 <- struct{}{}
+	}()
+	select {
+	case <-c1:
+		fmt.Println("c1 turn do something")
+	case <-c2:
+		fmt.Println("c2 turn do something")
+	}
+	fmt.Println("end")
+}
+
+func TestBasicSelectTimes(t *testing.T) {
+	c1 := make(chan interface{})
+	close(c1)
+	c2 := make(chan interface{})
+	close(c2)
+	cnt1 := 0
+	cnt2 := 0
+	for i := 0; i < 1000; i++ {
+		select {
+		case <-c1:
+			cnt1++
+		case <-c2:
+			cnt2++
+		}
+	}
+	fmt.Println("cnt1:", cnt1, "cnt2:", cnt2)
+}
+
+func TestBasicSelectWithTimeout(t *testing.T) {
+	c1 := make(chan interface{})
+	c2 := make(chan interface{})
+	go func() {
+		time.Sleep(2 * time.Second)
+		c1 <- struct{}{}
+	}()
+	go func() {
+		time.Sleep(2 * time.Second)
+		c2 <- struct{}{}
+	}()
+	select {
+	case <-c1:
+		fmt.Println("c1 turn do something")
+	case <-c2:
+		fmt.Println("c2 turn do something")
+	case <-time.After(1 * time.Second):
+		fmt.Println("time out")
+	}
+	fmt.Println("end")
 }
