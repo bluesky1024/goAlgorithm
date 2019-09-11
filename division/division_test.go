81ffc4c1eed34753bca6cecea7813f4470f85719
bluesky1024 <fangmeng_buaa@163.com>
2019-09-11 19:42:00 +0800

    旋转数组二分法找最小值

--- division/division_test.go
+++ division/division_test.go
@@ -13,3 +13,8 @@ func TestSearch(t *testing.T) {
 	nums := []int{-1, 0, 3, 5, 9, 12}
 	fmt.Println(Search(nums, 2))
 }
+
+func TestFindMin(t *testing.T) {
+	nums := []int{7, 0, 1, 2, 3, 5, 6}
+	fmt.Println(FindMin(nums))
+}
